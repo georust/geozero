ee6cb13ee7e70a83c0087b37ad60950ee9c23846
Pirmin Kalberer <pka@sourcepole.ch>
2020-05-25 21:27:23 +0200

    Make geos dependency optional

--- geozero-core/src/lib.rs
+++ geozero-core/src/lib.rs
@@ -1,6 +1,8 @@
 mod geojson_reader;
 mod geojson_writer;
+#[cfg(feature = "geos")]
 mod geos_reader;
+#[cfg(feature = "geos")]
 mod geos_writer;
 mod rustgeo_writer;
 pub mod svg;
@@ -19,6 +21,7 @@ pub mod wkt {
     pub use crate::wkt_writer::*;
 }
 
+#[cfg(feature = "geos")]
 pub mod geos {
     pub use crate::geos_reader::*;
     pub use crate::geos_writer::*;
