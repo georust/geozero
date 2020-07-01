3fa0ba0c812bdec58ee6a40da440a4539fc182bc
Pirmin Kalberer <pka@sourcepole.ch>
2020-07-01 23:39:14 +0200

    Add curve and surface WKB types

--- geozero-core/src/wkb_reader.rs
+++ geozero-core/src/wkb_reader.rs
@@ -9,43 +9,71 @@ enum WKBGeometryType {
     Point = 1,
     LineString = 2,
     Polygon = 3,
-    Triangle = 17,
     MultiPoint = 4,
     MultiLineString = 5,
     MultiPolygon = 6,
     GeometryCollection = 7,
+    CircularString = 8,
+    CompoundCurve = 9,
+    CurvePolygon = 10,
+    MultiCurve = 11,
+    MultiSurface = 12,
+    Curve = 13,
+    Surface = 14,
     PolyhedralSurface = 15,
-    TIN = 16,
+    Tin = 16,
+    Triangle = 17,
     PointZ = 1001,
     LineStringZ = 1002,
     PolygonZ = 1003,
-    Trianglez = 1017,
     MultiPointZ = 1004,
     MultiLineStringZ = 1005,
     MultiPolygonZ = 1006,
     GeometryCollectionZ = 1007,
+    CircularStringZ = 1008,
+    CompoundCurveZ = 1009,
+    CurvePolygonZ = 1010,
+    MultiCurveZ = 1011,
+    MultiSurfaceZ = 1012,
+    CurveZ = 1013,
+    SurfaceZ = 1014,
     PolyhedralSurfaceZ = 1015,
-    TINZ = 1016,
+    TinZ = 1016,
+    TriangleZ = 1017,
     PointM = 2001,
     LineStringM = 2002,
     PolygonM = 2003,
-    TriangleM = 2017,
     MultiPointM = 2004,
     MultiLineStringM = 2005,
     MultiPolygonM = 2006,
     GeometryCollectionM = 2007,
+    CircularStringM = 2008,
+    CompoundCurveM = 2009,
+    CurvePolygonM = 2010,
+    MultiCurveM = 2011,
+    MultiSurfaceM = 2012,
+    CurveM = 2013,
+    SurfaceM = 2014,
     PolyhedralSurfaceM = 2015,
-    TINM = 2016,
+    TinM = 2016,
+    TriangleM = 2017,
     PointZM = 3001,
     LineStringZM = 3002,
     PolygonZM = 3003,
-    TriangleZM = 3017,
     MultiPointZM = 3004,
     MultiLineStringZM = 3005,
     MultiPolygonZM = 3006,
     GeometryCollectionZM = 3007,
+    CircularStringZM = 3008,
+    CompoundCurveZM = 3009,
+    CurvePolygonZM = 3010,
+    MultiCurveZM = 3011,
+    MultiSurfaceZM = 3012,
+    CurveZM = 3013,
+    SurfaceZM = 3014,
     PolyhedralSurfaceZM = 3015,
     TinZM = 3016,
+    TriangleZM = 3017,
     // Extension to OGC spec
     Unknown = 255,
 }
@@ -56,23 +84,37 @@ impl WKBGeometryType {
             1 => WKBGeometryType::Point,
             2 => WKBGeometryType::LineString,
             3 => WKBGeometryType::Polygon,
-            17 => WKBGeometryType::Triangle,
             4 => WKBGeometryType::MultiPoint,
             5 => WKBGeometryType::MultiLineString,
             6 => WKBGeometryType::MultiPolygon,
             7 => WKBGeometryType::GeometryCollection,
+            8 => WKBGeometryType::CircularString,
+            9 => WKBGeometryType::CompoundCurve,
+            10 => WKBGeometryType::CurvePolygon,
+            11 => WKBGeometryType::MultiCurve,
+            12 => WKBGeometryType::MultiSurface,
+            13 => WKBGeometryType::Curve,
+            14 => WKBGeometryType::Surface,
             15 => WKBGeometryType::PolyhedralSurface,
-            16 => WKBGeometryType::TIN,
+            16 => WKBGeometryType::Tin,
+            17 => WKBGeometryType::Triangle,
             1001 => WKBGeometryType::PointZ,
             1002 => WKBGeometryType::LineStringZ,
             1003 => WKBGeometryType::PolygonZ,
-            1017 => WKBGeometryType::Trianglez,
             1004 => WKBGeometryType::MultiPointZ,
             1005 => WKBGeometryType::MultiLineStringZ,
             1006 => WKBGeometryType::MultiPolygonZ,
             1007 => WKBGeometryType::GeometryCollectionZ,
+            1008 => WKBGeometryType::CircularStringZ,
+            1009 => WKBGeometryType::CompoundCurveZ,
+            1010 => WKBGeometryType::CurvePolygonZ,
+            1011 => WKBGeometryType::MultiCurveZ,
+            1012 => WKBGeometryType::MultiSurfaceZ,
+            1013 => WKBGeometryType::CurveZ,
+            1014 => WKBGeometryType::SurfaceZ,
             1015 => WKBGeometryType::PolyhedralSurfaceZ,
-            1016 => WKBGeometryType::TINZ,
+            1016 => WKBGeometryType::TinZ,
+            1017 => WKBGeometryType::TriangleZ,
             2001 => WKBGeometryType::PointM,
             2002 => WKBGeometryType::LineStringM,
             2003 => WKBGeometryType::PolygonM,
@@ -81,18 +123,32 @@ impl WKBGeometryType {
             2005 => WKBGeometryType::MultiLineStringM,
             2006 => WKBGeometryType::MultiPolygonM,
             2007 => WKBGeometryType::GeometryCollectionM,
+            2008 => WKBGeometryType::CircularStringM,
+            2009 => WKBGeometryType::CompoundCurveM,
+            2010 => WKBGeometryType::CurvePolygonM,
+            2011 => WKBGeometryType::MultiCurveM,
+            2012 => WKBGeometryType::MultiSurfaceM,
+            2013 => WKBGeometryType::CurveM,
+            2014 => WKBGeometryType::SurfaceM,
             2015 => WKBGeometryType::PolyhedralSurfaceM,
-            2016 => WKBGeometryType::TINM,
+            2016 => WKBGeometryType::TinM,
             3001 => WKBGeometryType::PointZM,
             3002 => WKBGeometryType::LineStringZM,
             3003 => WKBGeometryType::PolygonZM,
-            3017 => WKBGeometryType::TriangleZM,
             3004 => WKBGeometryType::MultiPointZM,
             3005 => WKBGeometryType::MultiLineStringZM,
             3006 => WKBGeometryType::MultiPolygonZM,
             3007 => WKBGeometryType::GeometryCollectionZM,
+            3008 => WKBGeometryType::CircularStringZM,
+            3009 => WKBGeometryType::CompoundCurveZM,
+            3010 => WKBGeometryType::CurvePolygonZM,
+            3011 => WKBGeometryType::MultiCurveZM,
+            3012 => WKBGeometryType::MultiSurfaceZM,
+            3013 => WKBGeometryType::CurveZM,
+            3014 => WKBGeometryType::SurfaceZM,
             3015 => WKBGeometryType::PolyhedralSurfaceZM,
             3016 => WKBGeometryType::TinZM,
+            3017 => WKBGeometryType::TriangleZM,
             _ => WKBGeometryType::Unknown,
         }
     }
@@ -457,6 +513,32 @@ mod test {
             std::str::from_utf8(&wkt_data).unwrap(),
             "GEOMETRYCOLLECTION (POINT (10 10), POINT (30 30), LINESTRING (15 15, 20 20))"
         );
+
+        // SELECT 'CIRCULARSTRING(0 0,1 1,2 0)'::geometry
+        let ewkb = hex::decode("01080000000300000000000000000000000000000000000000000000000000F03F000000000000F03F00000000000000400000000000000000").unwrap();
+
+        // SELECT 'COMPOUNDCURVE (CIRCULARSTRING (0 0,1 1,2 0),(2 0,3 0))'::geometry
+        let ewkb = hex::decode("01090000000200000001080000000300000000000000000000000000000000000000000000000000F03F000000000000F03F000000000000004000000000000000000102000000020000000000000000000040000000000000000000000000000008400000000000000000").unwrap();
+
+        // SELECT 'CURVEPOLYGON(COMPOUNDCURVE(CIRCULARSTRING(0 0,1 1,2 0),(2 0,3 0,3 -1,0 -1,0 0)))'::geometry
+        let ewkb = hex::decode("010A0000000100000001090000000200000001080000000300000000000000000000000000000000000000000000000000F03F000000000000F03F0000000000000040000000000000000001020000000500000000000000000000400000000000000000000000000000084000000000000000000000000000000840000000000000F0BF0000000000000000000000000000F0BF00000000000000000000000000000000").unwrap();
+
+        // SELECT 'MULTICURVE(CIRCULARSTRING(0 0,1 1,2 0))'::geometry
+        let ewkb = hex::decode("010B0000000100000001080000000300000000000000000000000000000000000000000000000000F03F000000000000F03F00000000000000400000000000000000").unwrap();
+
+        // SELECT 'MULTISURFACE (CURVEPOLYGON (COMPOUNDCURVE (CIRCULARSTRING (0 0,1 1,2 0),(2 0,3 0,3 -1,0 -1,0 0))))'::geometry
+        let ewkb = hex::decode("010C00000001000000010A0000000100000001090000000200000001080000000300000000000000000000000000000000000000000000000000F03F000000000000F03F0000000000000040000000000000000001020000000500000000000000000000400000000000000000000000000000084000000000000000000000000000000840000000000000F0BF0000000000000000000000000000F0BF00000000000000000000000000000000").unwrap();
+
+        // Curve: LineString | CircularString | CompoundCurve
+
+        // SELECT 'POLYHEDRALSURFACE(((0 0 0,0 0 1,0 1 1,0 1 0,0 0 0)),((0 0 0,0 1 0,1 1 0,1 0 0,0 0 0)),((0 0 0,1 0 0,1 0 1,0 0 1,0 0 0)),((1 1 0,1 1 1,1 0 1,1 0 0,1 1 0)),((0 1 0,0 1 1,1 1 1,1 1 0,0 1 0)),((0 0 1,1 0 1,1 1 1,0 1 1,0 0 1)))'::geometry
+        let ewkb = hex::decode("010F000080060000000103000080010000000500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000F03F0000000000000000000000000000F03F000000000000F03F0000000000000000000000000000F03F0000000000000000000000000000000000000000000000000000000000000000010300008001000000050000000000000000000000000000000000000000000000000000000000000000000000000000000000F03F0000000000000000000000000000F03F000000000000F03F0000000000000000000000000000F03F0000000000000000000000000000000000000000000000000000000000000000000000000000000001030000800100000005000000000000000000000000000000000000000000000000000000000000000000F03F00000000000000000000000000000000000000000000F03F0000000000000000000000000000F03F00000000000000000000000000000000000000000000F03F00000000000000000000000000000000000000000000000001030000800100000005000000000000000000F03F000000000000F03F0000000000000000000000000000F03F000000000000F03F000000000000F03F000000000000F03F0000000000000000000000000000F03F000000000000F03F00000000000000000000000000000000000000000000F03F000000000000F03F0000000000000000010300008001000000050000000000000000000000000000000000F03F00000000000000000000000000000000000000000000F03F000000000000F03F000000000000F03F000000000000F03F000000000000F03F000000000000F03F000000000000F03F00000000000000000000000000000000000000000000F03F00000000000000000103000080010000000500000000000000000000000000000000000000000000000000F03F000000000000F03F0000000000000000000000000000F03F000000000000F03F000000000000F03F000000000000F03F0000000000000000000000000000F03F000000000000F03F00000000000000000000000000000000000000000000F03F").unwrap();
+
+        // SELECT 'TIN(((0 0 0,0 0 1,0 1 0,0 0 0)),((0 0 0,0 1 0,1 1 0,0 0 0)))'::geometry
+        let ewkb = hex::decode("0110000080020000000111000080010000000400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000F03F0000000000000000000000000000F03F0000000000000000000000000000000000000000000000000000000000000000011100008001000000040000000000000000000000000000000000000000000000000000000000000000000000000000000000F03F0000000000000000000000000000F03F000000000000F03F0000000000000000000000000000000000000000000000000000000000000000").unwrap();
+
+        // SELECT 'TRIANGLE((0 0,0 9,9 0,0 0))'::geometry
+        let ewkb = hex::decode("0111000000010000000400000000000000000000000000000000000000000000000000000000000000000022400000000000002240000000000000000000000000000000000000000000000000").unwrap();
     }
 
     #[test]
