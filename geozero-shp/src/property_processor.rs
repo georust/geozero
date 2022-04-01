515e0dffd2b46bd1eebf5b3e2a55e76e42e51ffb
Pirmin Kalberer <pka@sourcepole.ch>
2022-04-01 23:26:48 +0200

    cargo fmt

--- geozero-shp/src/property_processor.rs
+++ geozero-shp/src/property_processor.rs
@@ -9,17 +9,29 @@ impl FeatureProperties for ShapeRecord {
         let mut i = 0;
         for (name, value) in self.record.as_ref().iter() {
             let finish = match value {
-                FieldValue::Character(Some(val)) => processor.property(i, name, &ColumnValue::String(val))?,
-                FieldValue::Numeric(Some(val)) => processor.property(i, name, &ColumnValue::Double(*val))?,
-                FieldValue::Logical(Some(val)) => processor.property(i, name, &ColumnValue::Bool(*val))?,
+                FieldValue::Character(Some(val)) => {
+                    processor.property(i, name, &ColumnValue::String(val))?
+                }
+                FieldValue::Numeric(Some(val)) => {
+                    processor.property(i, name, &ColumnValue::Double(*val))?
+                }
+                FieldValue::Logical(Some(val)) => {
+                    processor.property(i, name, &ColumnValue::Bool(*val))?
+                }
                 FieldValue::Date(Some(_)) => {
                     let s = format!("{}", value);
                     processor.property(i, name, &ColumnValue::DateTime(&s))?
                 }
-                FieldValue::Float(Some(val)) => processor.property(i, name, &ColumnValue::Float(*val))?,
+                FieldValue::Float(Some(val)) => {
+                    processor.property(i, name, &ColumnValue::Float(*val))?
+                }
                 FieldValue::Integer(val) => processor.property(i, name, &ColumnValue::Int(*val))?,
-                FieldValue::Double(val) => processor.property(i, name, &ColumnValue::Double(*val))?,
-                FieldValue::Currency(val) => processor.property(i, name, &ColumnValue::Double(*val))?,
+                FieldValue::Double(val) => {
+                    processor.property(i, name, &ColumnValue::Double(*val))?
+                }
+                FieldValue::Currency(val) => {
+                    processor.property(i, name, &ColumnValue::Double(*val))?
+                }
                 FieldValue::DateTime(_) => {
                     let s = format!("{}", value);
                     processor.property(i, name, &ColumnValue::DateTime(&s))?
@@ -29,10 +41,12 @@ impl FeatureProperties for ShapeRecord {
                 | FieldValue::Numeric(None)
                 | FieldValue::Logical(None)
                 | FieldValue::Date(None)
-                | FieldValue::Float(None) => { continue; } // Ignore NULL values
+                | FieldValue::Float(None) => {
+                    continue; // Ignore NULL values
+                }
             };
             if finish {
-                return Ok(true)
+                return Ok(true);
             }
             i += 1;
         }
