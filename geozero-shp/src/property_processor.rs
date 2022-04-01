8c4569e3860ec92840c5d483e0ecb93512bc6840
Jay Cary <101132508+jcary741@users.noreply.github.com>
2022-04-01 11:13:26 -0400

    slightly cleaner code for property_processor.rs

--- geozero-shp/src/property_processor.rs
+++ geozero-shp/src/property_processor.rs
@@ -6,53 +6,36 @@ use geozero::{ColumnValue, FeatureProperties, PropertyProcessor};
 impl FeatureProperties for ShapeRecord {
     /// Process feature properties.
     fn process_properties<P: PropertyProcessor>(&self, processor: &mut P) -> Result<bool> {
-        let mut finish = false;
-        let mut null_offset = 0usize;
-        for (i, (name, value)) in self.record.as_ref().iter().enumerate() {
-            let i = i - null_offset;
-            match value {
-                FieldValue::Character(Some(val)) => {
-                    finish = processor.property(i, name, &ColumnValue::String(val))?;
-                }
-                FieldValue::Numeric(Some(val)) => {
-                    finish = processor.property(i, name, &ColumnValue::Double(*val))?;
-                }
-                FieldValue::Logical(Some(val)) => {
-                    finish = processor.property(i, name, &ColumnValue::Bool(*val))?;
-                }
+        let mut i = 0;
+        for (name, value) in self.record.as_ref().iter() {
+            let finish = match value {
+                FieldValue::Character(Some(val)) => processor.property(i, name, &ColumnValue::String(val))?,
+                FieldValue::Numeric(Some(val)) => processor.property(i, name, &ColumnValue::Double(*val))?,
+                FieldValue::Logical(Some(val)) => processor.property(i, name, &ColumnValue::Bool(*val))?,
                 FieldValue::Date(Some(_)) => {
                     let s = format!("{}", value);
-                    finish = processor.property(i, name, &ColumnValue::DateTime(&s))?;
+                    processor.property(i, name, &ColumnValue::DateTime(&s))?
                 }
-                FieldValue::Float(Some(val)) => {
-                    finish = processor.property(i, name, &ColumnValue::Float(*val))?;
-                }
-                FieldValue::Integer(val) => {
-                    finish = processor.property(i, name, &ColumnValue::Int(*val))?;
-                }
-                FieldValue::Double(val) => {
-                    finish = processor.property(i, name, &ColumnValue::Double(*val))?;
-                }
-                FieldValue::Currency(val) => {
-                    finish = processor.property(i, name, &ColumnValue::Double(*val))?;
-                }
+                FieldValue::Float(Some(val)) => processor.property(i, name, &ColumnValue::Float(*val))?,
+                FieldValue::Integer(val) => processor.property(i, name, &ColumnValue::Int(*val))?,
+                FieldValue::Double(val) => processor.property(i, name, &ColumnValue::Double(*val))?,
+                FieldValue::Currency(val) => processor.property(i, name, &ColumnValue::Double(*val))?,
                 FieldValue::DateTime(_) => {
                     let s = format!("{}", value);
-                    finish = processor.property(i, name, &ColumnValue::DateTime(&s))?;
+                    processor.property(i, name, &ColumnValue::DateTime(&s))?
                 }
-                FieldValue::Memo(val) => {
-                    finish = processor.property(i, name, &ColumnValue::String(val))?;
-                }
+                FieldValue::Memo(val) => processor.property(i, name, &ColumnValue::String(val))?,
                 FieldValue::Character(None)
                 | FieldValue::Numeric(None)
                 | FieldValue::Logical(None)
                 | FieldValue::Date(None)
-                | FieldValue::Float(None) => { null_offset += 1 } // Ignore NULL values
-            }
+                | FieldValue::Float(None) => { continue; } // Ignore NULL values
+            };
             if finish {
-                break;
+                return Ok(true)
             }
+            i += 1;
         }
-        Ok(finish)
+        Ok(false)
     }
 }
