2a5253cbef398cb87a5720eca95cf61a31763add
Kyle Barron <kylebarron2@gmail.com>
2022-07-17 12:42:22 -0600

    add test and test data; impl on BinaryArray directly

--- geozero/src/arrow/geoarrow_reader.rs
+++ geozero/src/arrow/geoarrow_reader.rs
@@ -1,24 +1,22 @@
 use crate::error::Result;
 use crate::wkb::wkb_reader::{process_wkb_geom_n, read_wkb_header};
 use crate::{GeomProcessor, GeozeroGeometry};
-use arrow2::array::BinaryArray;
+use arrow2::array::{BinaryArray, Offset};
 
-/// GeoArrow WKB reader
-// TODO: BinaryArray is generic over the type used for offsets: i32 and i64. This should not be
-// hardcoded as i32
-#[derive(Debug)]
-pub struct GeoArrowWkb(pub BinaryArray<i32>);
-
-impl GeozeroGeometry for GeoArrowWkb {
+impl GeozeroGeometry for BinaryArray<i32> {
     fn process_geom<P: GeomProcessor>(&self, processor: &mut P) -> Result<()> {
-        // TODO: how does this fn know which index to pick?
-        // Or is this fn intended to work on a single geometry?
-        process_geoarrow_wkb_geom(&self.0, processor)
+        process_geoarrow_wkb_geom(self, processor)
     }
 }
 
-pub fn process_geoarrow_wkb_geom(
-    array: &BinaryArray<i32>,
+impl GeozeroGeometry for BinaryArray<i64> {
+    fn process_geom<P: GeomProcessor>(&self, processor: &mut P) -> Result<()> {
+        process_geoarrow_wkb_geom(self, processor)
+    }
+}
+
+pub fn process_geoarrow_wkb_geom<T: Offset>(
+    array: &BinaryArray<T>,
     processor: &mut impl GeomProcessor,
 ) -> Result<()> {
     let array_len = array.len();
@@ -33,4 +31,34 @@ pub fn process_geoarrow_wkb_geom(
     processor.geometrycollection_end(array_len - 1)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::wkt::conversion::ToWkt;
+    use arrow2::io::ipc::read;
+    use std::fs::File;
+
+    #[test]
+    fn multipoly_file() -> arrow2::error::Result<()> {
+        let mut file = File::open("tests/data/countries.arrow")?;
+        let metadata = read::read_file_metadata(&mut file)?;
+        let mut reader = read::FileReader::new(file, metadata, None);
+
+        let columns = reader.next().unwrap()?;
+
+        let array = &columns.arrays()[2];
+        let wkbarr = array.as_any().downcast_ref::<BinaryArray<i32>>().unwrap();
+        let wkt = wkbarr.to_wkt().unwrap();
+        assert_eq!(
+            &wkt[0..100],
+            "GEOMETRYCOLLECTION(MULTIPOLYGON(((-59.572095 -80.040179,-59.865849 -80.549657,-60.159656 -81.000327,"
+        );
+        assert_eq!(
+            &wkt[wkt.len()-100..],
+            "-51.5,-58.55 -51.1,-57.75 -51.55,-58.05 -51.9,-59.4 -52.2,-59.85 -51.85,-60.7 -52.3,-61.2 -51.85))))"
+        );
+        Ok(())
+    }
 }
