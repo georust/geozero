2245b2787716fb1d3f17a78c67a845b90eb26db4
Corey Farwell <coreyf@rwell.org>
2022-09-04 22:22:08 -0400

    -

--- geozero/src/gpx/gpx_reader.rs
+++ geozero/src/gpx/gpx_reader.rs
@@ -1,40 +1,52 @@
-use crate::error::{GeozeroError, Result};
 use std::io;
 
 pub fn read_gpx<R: io::Read, P: crate::GeomProcessor>(
     reader: &mut R,
     processor: &mut P,
-) -> Result<()> {
+) -> crate::error::Result<()> {
     let gpx_reader = gpx::read(reader).unwrap(); // TODO;
-    let mut index = 0; // WHAT IS THIS FOR
 
     // Waypoints
-    processor.multipoint_begin(gpx_reader.waypoints.len(), index)?;
+    processor.multipoint_begin(gpx_reader.waypoints.len(), 0)?;
     for waypoint in &gpx_reader.waypoints {
         let point = waypoint.point();
-        processor.xy(point.x(), point.y(), index)?;
+        processor.xy(point.x(), point.y(), 0)?;
     }
-    processor.multipoint_end(index)?;
+    processor.multipoint_end(0)?;
 
     // Tracks
-    processor.multilinestring_begin(todo!(), index)?; // TODO: what is the right size
-    for track in &gpx_reader.tracks {
-        // TODO: is this multi<multi<linestring>>?
+    for (index, track) in gpx_reader.tracks.iter().enumerate() {
+        processor.multilinestring_begin(track.segments.len(), index)?;
         for segment in track.segments.iter() {
-            processor.linestring_begin(false, todo!(), index)?; // TODO: what is the right size
-            for waypoint in segment.points {
+            processor.linestring_begin(false, segment.points.len(), index)?;
+            for waypoint in &segment.points {
                 let point = waypoint.point();
                 processor.xy(point.x(), point.y(), index)?;
             }
             processor.linestring_end(false, index)?;
         }
+        processor.multilinestring_end(index)?;
     }
-    processor.multilinestring_end(index)?;
 
     // Routes
-    todo!()
+    processor.multilinestring_begin(gpx_reader.routes.len(), 0)?;
+    for (index, route) in gpx_reader.routes.iter().enumerate() {
+        processor.linestring_begin(false, route.points.len(), index)?;
+        for waypoint in route.points.iter() {
+            let point = waypoint.point();
+            processor.xy(point.x(), point.y(), index)?;
+        }
+        processor.linestring_end(false, index)?;
+    }
+    processor.multilinestring_end(0)?;
+
+    Ok(())
 }
 
-fn process_point() {
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_basic() {
 
+    }
 }
--- geozero/src/gpx/mod.rs
+++ geozero/src/gpx/mod.rs
@@ -1,1 +1,3 @@
 mod gpx_reader;
+
+pub use gpx_reader::read_gpx;
