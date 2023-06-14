2603b45a26e058fd2b1862665229aaef8ee60888
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-06-14 14:30:31 +0100

    remove lifetimes

--- geozero/src/mvt/mvt_error.rs
+++ geozero/src/mvt/mvt_error.rs
@@ -2,15 +2,15 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum MvtError<'a> {
+pub enum MvtError {
     #[error("invalid feature.tags length: {0}")]
     InvalidFeatureTagsLength(usize),
     #[error("invalid key index {0}")]
-    InvalidKeyIndex(&'a u32),
+    InvalidKeyIndex(u32),
     #[error("invalid value index {0}")]
-    InvalidValueIndex(&'a u32),
+    InvalidValueIndex(u32),
     #[error("unsupported value type for key {0}")]
-    UnsupportedKeyValueType(&'a String),
+    UnsupportedKeyValueType(String),
     #[error("geometry format")]
     GeometryFormat,
     #[error("too few coordinates in line or ring")]
