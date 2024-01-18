09aeba0f4aa9a0364eddad61ca1e981794bd8a04
Kyle Barron <kylebarron2@gmail.com>
2024-01-17 21:45:46 -0500

    Address comment

--- geozero/src/geojson/geojson_line_writer.rs
+++ geozero/src/geojson/geojson_line_writer.rs
@@ -8,33 +8,55 @@ use crate::{FeatureProcessor, GeomProcessor, PropertyProcessor};
 ///
 /// See <https://jsonlines.org>
 pub struct GeoJsonLineWriter<W: Write> {
+    /// We use a count of the number of contexts entered to decide when to add a newline character
+    /// and finish a line. The [newline-delimited GeoJSON
+    /// spec](https://datatracker.ietf.org/doc/html/rfc8142) defines that any type of GeoJSON
+    /// objects can be written as an object on a single line. Therefore, we can't solely add
+    /// newlines in `feature_end`. If the object on this line is a Point geometry, then we need to
+    /// add a newline character in `point_end`, because `feature_end` will never be called.
+    ///
+    /// Note that this approach is not resilient to malformed input. If the number of begin and end
+    /// calls do not match, newline characters will not be correctly added.
+    open_contexts: usize,
     line_writer: GeoJsonWriter<W>,
 }
 
 impl<W: Write> GeoJsonLineWriter<W> {
     pub fn new(out: W) -> Self {
         Self {
+            open_contexts: 0,
             line_writer: GeoJsonWriter::new(out),
         }
+    }
+
+    fn write_newline(&mut self) -> Result<()> {
+        self.line_writer.out.write_all(b"\n")?;
+        Ok(())
+    }
+
+    fn begin_context(&mut self) {
+        self.open_contexts += 1;
+    }
+
+    fn end_context(&mut self) -> Result<()> {
+        self.open_contexts -= 1;
+        if self.open_contexts == 0 {
+            self.write_newline()?;
+        }
+        Ok(())
     }
 }
 
 impl<W: Write> FeatureProcessor for GeoJsonLineWriter<W> {
-    fn dataset_begin(&mut self, _name: Option<&str>) -> Result<()> {
-        Ok(())
-    }
-
-    fn dataset_end(&mut self) -> Result<()> {
-        Ok(())
-    }
-
     fn feature_begin(&mut self, _idx: u64) -> Result<()> {
-        self.line_writer.feature_begin(0)
+        self.begin_context();
+        self.line_writer.feature_begin(0)?;
+        Ok(())
     }
 
     fn feature_end(&mut self, _idx: u64) -> Result<()> {
         self.line_writer.feature_end(0)?;
-        self.line_writer.out.write_all(b"\n")?;
+        self.end_context()?;
         Ok(())
     }
 
@@ -78,63 +100,79 @@ impl<W: Write> GeomProcessor for GeoJsonLineWriter<W> {
     }
 
     fn empty_point(&mut self, idx: usize) -> Result<()> {
-        self.line_writer.empty_point(idx)
+        self.begin_context();
+        self.line_writer.empty_point(idx)?;
+        self.end_context()
     }
 
     fn point_begin(&mut self, idx: usize) -> Result<()> {
+        self.begin_context();
         self.line_writer.point_begin(idx)
     }
 
     fn point_end(&mut self, idx: usize) -> Result<()> {
-        self.line_writer.point_end(idx)
+        self.line_writer.point_end(idx)?;
+        self.end_context()
     }
 
     fn multipoint_begin(&mut self, size: usize, idx: usize) -> Result<()> {
+        self.begin_context();
         self.line_writer.multipoint_begin(size, idx)
     }
 
     fn multipoint_end(&mut self, idx: usize) -> Result<()> {
-        self.line_writer.multipoint_end(idx)
+        self.line_writer.multipoint_end(idx)?;
+        self.end_context()
     }
 
     fn linestring_begin(&mut self, tagged: bool, size: usize, idx: usize) -> Result<()> {
+        self.begin_context();
         self.line_writer.linestring_begin(tagged, size, idx)
     }
 
     fn linestring_end(&mut self, tagged: bool, idx: usize) -> Result<()> {
-        self.line_writer.linestring_end(tagged, idx)
+        self.line_writer.linestring_end(tagged, idx)?;
+        self.end_context()
     }
 
     fn multilinestring_begin(&mut self, size: usize, idx: usize) -> Result<()> {
+        self.begin_context();
         self.line_writer.multilinestring_begin(size, idx)
     }
 
     fn multilinestring_end(&mut self, idx: usize) -> Result<()> {
-        self.line_writer.multilinestring_end(idx)
+        self.line_writer.multilinestring_end(idx)?;
+        self.end_context()
     }
 
     fn polygon_begin(&mut self, tagged: bool, size: usize, idx: usize) -> Result<()> {
+        self.begin_context();
         self.line_writer.polygon_begin(tagged, size, idx)
     }
 
     fn polygon_end(&mut self, tagged: bool, idx: usize) -> Result<()> {
-        self.line_writer.polygon_end(tagged, idx)
+        self.line_writer.polygon_end(tagged, idx)?;
+        self.end_context()
     }
 
     fn multipolygon_begin(&mut self, size: usize, idx: usize) -> Result<()> {
+        self.begin_context();
         self.line_writer.multipolygon_begin(size, idx)
     }
 
     fn multipolygon_end(&mut self, idx: usize) -> Result<()> {
-        self.line_writer.multipolygon_end(idx)
+        self.line_writer.multipolygon_end(idx)?;
+        self.end_context()
     }
 
     fn geometrycollection_begin(&mut self, size: usize, idx: usize) -> Result<()> {
+        self.begin_context();
         self.line_writer.geometrycollection_begin(size, idx)
     }
 
     fn geometrycollection_end(&mut self, idx: usize) -> Result<()> {
-        self.line_writer.geometrycollection_end(idx)
+        self.line_writer.geometrycollection_end(idx)?;
+        self.end_context()
     }
 }
 
@@ -160,7 +198,7 @@ mod tests {
         assert!(
             read_geojson_lines(input.as_bytes(), &mut GeoJsonLineWriter::new(&mut out)).is_ok()
         );
-        assert_json_eq(&out, input);
+        assert_json_lines_eq(&out, input);
     }
 
     #[test]
@@ -178,7 +216,6 @@ mod tests {
 
     fn assert_json_lines_eq(a: &[u8], b: &str) {
         let a = std::str::from_utf8(a).unwrap();
-        dbg!(a);
         a.lines().zip(b.lines()).for_each(|(a_line, b_line)| {
             dbg!(a);
 
