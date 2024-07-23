a95c3328c09c9978b17471694c41d37d754d2704
Kyle Barron <kyle@developmentseed.org>
2024-07-23 15:25:09 -0400

    Update for latest wkt

--- geozero/src/wkt/wkt_reader.rs
+++ geozero/src/wkt/wkt_reader.rs
@@ -3,7 +3,6 @@ use crate::{FeatureProcessor, GeomProcessor, GeozeroDatasource, GeozeroGeometry}
 
 use std::io::Read;
 use wkt::types::{Coord, LineString, Polygon};
-use wkt::Geometry;
 
 /// A wrapper around a WKT String or String slice.
 #[derive(Debug)]
@@ -78,22 +77,22 @@ pub fn read_wkt<R: Read, P: GeomProcessor>(reader: &mut R, processor: &mut P) ->
     let mut wkt_string = String::new();
     reader.read_to_string(&mut wkt_string)?;
     let wkt = wkt::Wkt::from_str(&wkt_string).map_err(|e| GeozeroError::Geometry(e.to_string()))?;
-    process_wkt_geom(&wkt.item, processor)
+    process_wkt_geom(&wkt, processor)
 }
 
 /// Process WKT geometry
-fn process_wkt_geom<P: GeomProcessor>(geometry: &Geometry<f64>, processor: &mut P) -> Result<()> {
+fn process_wkt_geom<P: GeomProcessor>(geometry: &wkt::Wkt<f64>, processor: &mut P) -> Result<()> {
     process_wkt_geom_n(geometry, 0, processor)
 }
 
 pub(crate) fn process_wkt_geom_n<P: GeomProcessor>(
-    geometry: &Geometry<f64>,
+    geometry: &wkt::Wkt<f64>,
     idx: usize,
     processor: &mut P,
 ) -> Result<()> {
     let multi_dim = processor.multi_dim();
     match geometry {
-        Geometry::Point(g) => {
+        wkt::Wkt::Point(g) => {
             if let Some(ref coord) = g.0 {
                 processor.point_begin(idx)?;
                 process_coord(coord, multi_dim, 0, processor)?;
@@ -102,7 +101,7 @@ pub(crate) fn process_wkt_geom_n<P: GeomProcessor>(
                 processor.empty_point(idx)
             }
         }
-        Geometry::MultiPoint(g) => {
+        wkt::Wkt::MultiPoint(g) => {
             processor.multipoint_begin(g.0.len(), idx)?;
             let multi_dim1 = processor.multi_dim();
             for (idxc, point) in g.0.iter().enumerate() {
@@ -116,23 +115,23 @@ pub(crate) fn process_wkt_geom_n<P: GeomProcessor>(
             }
             processor.multipoint_end(idx)
         }
-        Geometry::LineString(g) => process_linestring(g, true, idx, processor),
-        Geometry::MultiLineString(g) => {
+        wkt::Wkt::LineString(g) => process_linestring(g, true, idx, processor),
+        wkt::Wkt::MultiLineString(g) => {
             processor.multilinestring_begin(g.0.len(), idx)?;
             for (idxc, linestring) in g.0.iter().enumerate() {
                 process_linestring(linestring, false, idxc, processor)?;
             }
             processor.multilinestring_end(idx)
         }
-        Geometry::Polygon(g) => process_polygon(g, true, idx, processor),
-        Geometry::MultiPolygon(g) => {
+        wkt::Wkt::Polygon(g) => process_polygon(g, true, idx, processor),
+        wkt::Wkt::MultiPolygon(g) => {
             processor.multipolygon_begin(g.0.len(), idx)?;
             for (idx2, polygon) in g.0.iter().enumerate() {
                 process_polygon(polygon, false, idx2, processor)?;
             }
             processor.multipolygon_end(idx)
         }
-        Geometry::GeometryCollection(g) => {
+        wkt::Wkt::GeometryCollection(g) => {
             processor.geometrycollection_begin(g.0.len(), idx)?;
             for (idx2, geometry) in g.0.iter().enumerate() {
                 process_wkt_geom_n(geometry, idx2, processor)?;
